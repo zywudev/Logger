bf4eaf15bfce2d0a0f1696bbbed2acecf54772d6
zywudev <zywu.dev@gmail.com>
2018-06-29 16:32:36 +0800

    init

---
+++ library/src/main/java/com/wuzy/logger/CrashHandler.java
@@ -0,0 +1,22 @@
+package com.wuzy.logger;
+
+/**
+ * Created by wuzy on 2018/06/27
+ */
+
+public class CrashHandler implements Thread.UncaughtExceptionHandler {
+
+    private Thread.UncaughtExceptionHandler mDefaultExceptionHandler;
+
+    public CrashHandler() {
+        this.mDefaultExceptionHandler = Thread.getDefaultUncaughtExceptionHandler();
+    }
+
+    @Override
+    public void uncaughtException(Thread thread, Throwable throwable) {
+
+        L.e(throwable);
+        mDefaultExceptionHandler.uncaughtException(thread,throwable);
+
+    }
+}
